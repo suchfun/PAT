409b0225773aefc0f47ce3fced6b9969e2e4fd97
suchfun <846952972@qq.com>
2017-01-17 19:30:19 +0800

    1018

--- pat1_1.c
+++ pat1_1.c
@@ -665,4 +665,69 @@
 //    printf(" %d",temp);
 //    return 0;
 //}
+/*1018*/
+//int main(){
+//	int n,i;
+//	scanf("%d",&n);
+//	getchar();
+//	int jia[6]={0,0,0,0,0,0};
+//	int yi[6]={0,0,0,0,0,0};//分别存储胜、平、负、布、锤子、剪刀的个数
+//	char result[100001][4];
+//	for(i=0;i<n;++i){
+//		gets(result[i]);
+//	}
+//	for(i=0;i<n;++i){
+//		if((result[i][0]=='B'&&result[i][2]=='B')||(result[i][0]=='C'&&result[i][2]=='C')||(result[i][0]=='J'&&result[i][2]=='J')){
+//			++jia[1];
+//			++yi[1];	
+//		}
+//		else if(result[i][0]=='B'&&result[i][2]=='C'){
+//			++jia[0];++jia[3];
+//			++yi[2];			
+//		}
+//		else if(result[i][0]=='B'&&result[i][2]=='J'){
+//			++jia[2];
+//			++yi[0];++yi[5];			
+//		}
+//		else if(result[i][0]=='C'&&result[i][2]=='B'){
+//			++jia[2];
+//			++yi[0];++yi[3];			
+//		}
+//		else if(result[i][0]=='C'&&result[i][2]=='J'){
+//			++jia[0];++jia[4];
+//			++yi[2];			
+//		}
+//		else if(result[i][0]=='J'&&result[i][2]=='B'){
+//			++jia[0];++jia[5];
+//			++yi[2];			
+//		}
+//		else if(result[i][0]=='J'&&result[i][2]=='C'){
+//			++jia[2];
+//			++yi[0];++yi[4];			
+//		}
+//	}
+//	printf("%d %d %d\n",jia[0],jia[1],jia[2]);
+//	printf("%d %d %d\n",yi[0],yi[1],yi[2]);
+//	char BCM[4]={'B','C','J'};
+//	int max=0;
+//	max=jia[3];
+//	int index=3;
+//	for(i=4;i<6;++i){
+//		if(max<jia[i]){
+//			max=jia[i];
+//			index=i;
+//		}
+//	}
+//	printf("%c ",BCM[index-3]);
+//	for(i=4,index=3,max=yi[3];i<6;++i){
+//		if(max<yi[i]){
+//			max=yi[i];
+//			index=i;
+//		}
+//	}
+//	printf("%c",BCM[index-3]);
+//	return 0;	
+//}
+
+
 

61dbffde0e02a55fc1e268711ddf2353437cea6d
suchfun <846952972@qq.com>
2017-02-07 22:22:14 +0800

    写prime算法过程中想到cpp中关于&的用法

---
+++ about&.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+#define N 5
+
+typedef struct MGraph{
+	int edges[N][N];
+	int n,e;
+}MGraph;
+
+
+
+void initMGraph(int &a){
+	a=7;
+}
+
+int	prime(MGraph G,int v0){
+	int sum=0;
+	
+}
+
+int main(){
+	int a;
+	initMGraph(a);
+	printf("%d",a);
+	getchar();
+	 
+	return 0;
+} 
